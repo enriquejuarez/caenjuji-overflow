ec4cb4fa182c63ffdf4a25341487e4d8d79879ce
Carlos Enrique <caenjuji@gmail.com>
2020-10-14 10:38:43 -0500

    Add express and configuration initial for api

---
+++ server/app.js
@@ -0,0 +1,7 @@
+import express from 'express'
+import {question} from './routes'
+
+const app = express()
+app.use('/api/questions', question)
+
+export default app
---
+++ server/routes/question.js
@@ -0,0 +1,28 @@
+import express from 'express'
+
+const app = express.Router()
+
+const question = {
+  _id: 1,
+  title: '¿Cómo reutilizar un componente en Angular?',
+  description: 'Mire..',
+  createdAt: new Date(),
+  icon: 'devicon-android-plain',
+  answer: [],
+  user: {
+    firstName: 'Carlos',
+    lastName: 'Juarez',
+    email: 'caenjuji@gmail.com',
+    password: '123456789'
+  }
+}
+
+const questions = new Array(10).fill(question)
+
+// /api/questions
+app.get('/', (req, res) => res.status(200).json(questions))
+
+// /api/questions:id
+app.get('/:id', (req, res) => res.status(200).json(question))
+
+export default app
